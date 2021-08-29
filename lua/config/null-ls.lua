742538117d8c0efa9300d5ee9eba11ea5387efd5
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2021-08-29 16:18:26 +0200

    Improve conditional python formatting

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -1,12 +1,35 @@
 local nls = require("null-ls")
 local lspconfig = require("lspconfig")
 
+local black = {
+	extra_args = function(params)
+		return params.bufname:match("kialo") and { "-l 120" } or { "-l 80" }
+	end,
+}
+
+local flake8 = {
+	args = function(params)
+		return {
+			"--format",
+			"default",
+			"--config",
+			params.bufname:match("kialo") and os.getenv("KIALO_ROOT") .. "/backend/.flake8" or ".flake8",
+			"--stdin-display-name",
+			"$FILENAME",
+			"-",
+		}
+	end,
+}
+
 return {
 	setup = function(on_attach)
 		nls.config({
 			sources = {
+				nls.builtins.code_actions.gitsigns,
 				nls.builtins.diagnostics.eslint.with({ command = "eslint_d" }),
+				nls.builtins.diagnostics.flake8.with(flake8),
 				nls.builtins.diagnostics.hadolint,
+				nls.builtins.formatting.black.with(black),
 				nls.builtins.formatting.eslint_d,
 				nls.builtins.formatting.gofmt,
 				nls.builtins.formatting.goimports,
@@ -16,7 +39,6 @@ return {
 				nls.builtins.formatting.shfmt,
 				nls.builtins.formatting.sqlformat,
 				nls.builtins.formatting.stylua,
-				nls.builtins.code_actions.gitsigns,
 			},
 			save_after_format = false,
 		})
