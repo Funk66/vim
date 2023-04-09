8cc7daf9e72337b3ba6509d2c8012cc9250c2fe6
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2023-04-09 11:06:08 +0200

    Add keymaps for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,6 +1,34 @@
+function DiffviewToggle()
+  local lib = require("diffview.lib")
+  local view = lib.get_current_view()
+  if view then
+    vim.cmd(":DiffviewClose")
+  else
+    vim.cmd(":DiffviewOpen")
+  end
+end
+
 return {
   "sindrets/diffview.nvim",
-  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-  keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+  keys = {
+    {
+      "<leader>gd",
+      function()
+        DiffviewToggle()
+      end,
+      desc = "Toggle diff view",
+    },
+    {
+      "<leader>gf",
+      "<cmd>DiffviewFileHistory %<cr>",
+      desc = "File git history",
+    },
+    {
+      "<leader>gm",
+      "<cmd>DiffviewOpen master...HEAD<cr>",
+      desc = "Master diff",
+    },
+  },
   config = true,
 }
