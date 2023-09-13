3ceb15ecdbe928069d9b41b3314f15a2d1eb1789
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2023-09-13 09:42:17 +0200

    Fix mypy configuration

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,9 +6,13 @@ local black = {
 
 local mypy = {
   extra_args = function(params)
-    local python_path = os.getenv("VIRTUAL_ENV") or "/usr"
     return params.bufname:match("kialo")
-        and { "--config-file", "backend/mypy.ini", "--python-executable", python_path .. "/bin/python3" }
+        and {
+          "--config-file",
+          params.root .. "/backend/mypy.ini",
+          "--python-executable",
+          os.getenv("VIRTUAL_ENV") .. "/bin/python3",
+        }
       or {}
   end,
 }
@@ -21,25 +25,17 @@ local isort = {
 
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  opts = function()
+  opts = function(_, opts)
     local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        nls.builtins.code_actions.gitsigns,
-        nls.builtins.diagnostics.actionlint,
-        nls.builtins.diagnostics.eslint.with({ command = "eslint_d" }),
-        nls.builtins.diagnostics.mypy.with(mypy),
-        nls.builtins.formatting.black.with(black),
-        nls.builtins.formatting.eslint_d,
-        nls.builtins.formatting.gofmt,
-        nls.builtins.formatting.goimports,
-        nls.builtins.formatting.isort.with(isort),
-        nls.builtins.formatting.prettierd,
-        nls.builtins.formatting.shfmt,
-        nls.builtins.formatting.sqlformat,
-        nls.builtins.formatting.stylua,
-      },
-    }
+    vim.list_extend(opts.sources, {
+      nls.builtins.code_actions.gitsigns,
+      nls.builtins.diagnostics.actionlint,
+      nls.builtins.diagnostics.eslint.with({ command = "eslint_d" }),
+      nls.builtins.diagnostics.mypy.with(mypy),
+      nls.builtins.formatting.black.with(black),
+      nls.builtins.formatting.eslint_d,
+      nls.builtins.formatting.isort.with(isort),
+      nls.builtins.formatting.sqlformat,
+    })
   end,
 }
