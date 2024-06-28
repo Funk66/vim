765db852a143f5ff9b7b6e591fd209ea45b69134
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2024-06-28 17:41:43 +0200

    Remove gitsigns actions

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -16,7 +16,6 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls")
     vim.list_extend(opts.sources, {
-      nls.builtins.code_actions.gitsigns,
       nls.builtins.diagnostics.actionlint,
       nls.builtins.diagnostics.mypy.with(mypy),
       nls.builtins.formatting.sqlfmt,
