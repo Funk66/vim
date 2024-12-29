0d09209a0cd0461264e1c9303505ab244e67d458
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2024-12-29 09:41:31 +0100

    Configure k8s-schema-merger schemas for yaml linting

--- lua/plugins/yaml.lua
+++ lua/plugins/yaml.lua
@@ -1,35 +1,22 @@
 return {
   {
-    "astephanh/yaml-companion.nvim",
-    branch = "kubernetes_crd_detection",
-    ft = { "yaml" },
-    dependencies = {
-      { "neovim/nvim-lspconfig" },
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    config = function(_, opts)
-      local cfg = require("yaml-companion").setup(opts)
-      require("lspconfig")["yamlls"].setup(cfg)
-      require("telescope").load_extension("yaml_schema")
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      opts.sections.lualine_z = {
-        {
-          function()
-            local schema = require("yaml-companion").get_buf_schema(0)
-            if schema.result[1].name == "none" then
-              return ""
-            end
-            return schema.result[1].name
-          end,
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = {
+                ["/Users/guillermo/.local/share/k8s-schema-merger/all.json"] = {
+                  "base/*/*.y*ml",
+                  "!base/*/kustomization.y*ml",
+                  "manifests/*/*/*.y*ml",
+                },
+              },
+            },
+          },
         },
-        opts.sections.lualine_z,
-      }
-    end,
+      },
+    },
   },
 }
