7361a7ebdbbca9b02f5d94baa66eb25ef575701a
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2025-01-03 12:42:34 +0100

    Show dotfiles in neo-tree

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -4,5 +4,12 @@ return {
     keys = {
       { "<leader>fx", "<cmd>Neotree focus<cr>", desc = "Focus NeoTree (root dir)", silent = true, mode = { "n" } },
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
   },
 }
