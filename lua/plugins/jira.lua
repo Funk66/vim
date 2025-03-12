d0465ce06e3214d65c1c61e31e87ad92dcd37302
Guillermo Guirao Aguilar <ggaguilar@gmail.com>
2025-03-12 22:34:07 +0100

    Improve jira settings

--- lua/plugins/jira.lua
+++ lua/plugins/jira.lua
@@ -1,14 +1,25 @@
 return {
-  "Funk66/jira.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("jira").setup()
-  end,
-  cond = function()
-    return vim.env.JIRA_API_TOKEN ~= nil
-  end,
-  keys = {
-    { "<leader>jv", ":JiraView<cr>", desc = "View Jira issue", silent = true },
-    { "<leader>jo", ":JiraOpen<cr>", desc = "Open Jira issue in browser", silent = true },
+  {
+    "Funk66/jira.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("jira").setup()
+    end,
+    cond = function()
+      return vim.env.JIRA_API_TOKEN ~= nil
+    end,
+    keys = {
+      { "<leader>jv", ":JiraView<cr>", desc = "View Jira issue", silent = true },
+      { "<leader>jo", ":JiraOpen<cr>", desc = "Open Jira issue in browser", silent = true },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>j", group = "Jira", icon = { icon = "", color = "blue" } },
+      },
+    },
   },
 }
